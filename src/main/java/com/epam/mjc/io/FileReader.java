bd116820d8896f006a3c10058118eadb33b26d13
sergruff <sergruff@gmail.com>
2023-03-02 01:13:10 +0100

    s1m6task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder builder = new StringBuilder();
+        try (FileInputStream fileInputStream = new FileInputStream(file)){
+            int ch;
+            while ((ch=fileInputStream.read())!=-1){
+                builder.append(Character.toChars(ch));
+            }
+        }
+        catch (FileNotFoundException ex){
+            System.err.print(ex);
+        }
+        catch (IOException ex){
+            System.err.print(ex);
+        }
+        String[] contents = builder.toString().split("\n");
+        return new Profile(contents[0].substring(6).strip(),
+                Integer.parseInt(contents[1].substring(5).strip()),
+                contents[2]=contents[2].substring(7).strip(),
+                Long.parseLong(contents[3]=contents[3].substring(7).strip()));
     }
 }
