66e07c2029c2e6ac1eb97880cd57143e902e4625
sergruff <sergruff@gmail.com>
2023-03-02 11:30:26 +0100

    s1m6task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -4,10 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 
 public class FileReader {
-
+    private static final Logger log = Logger.getLogger(FileReader.class.getName());
     public Profile getDataFromFile(File file) {
         StringBuilder builder = new StringBuilder();
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
@@ -16,9 +17,9 @@ public class FileReader {
                 builder.append(Character.toChars(ch));
             }
         } catch (FileNotFoundException ex) {
-
+            log.info(ex.getMessage()+ " File not found");
         } catch (IOException ex) {
-
+            log.info(ex.getMessage()+ " IO exception");
         }
         String[] contents = builder.toString().split("\n");
         return new Profile(contents[0].substring(6).strip(),
