cf5da05f80a9da5130086bc571415be7a3b1c008
sergruff <sergruff@gmail.com>
2023-03-02 11:20:09 +0100

    s1m6task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -16,20 +16,15 @@ public class FileReader {
                 builder.append(Character.toChars(ch));
             }
         } catch (FileNotFoundException ex) {
-            System.err.print(ex);
+
         } catch (IOException ex) {
-            System.err.print(ex);
+
         }
         String[] contents = builder.toString().split("\n");
         return new Profile(contents[0].substring(6).strip(),
                 Integer.parseInt(contents[1].substring(5).strip()),
-                contents[2] = contents[2].substring(7).strip(),
-                Long.parseLong(contents[3] = contents[3].substring(7).strip()));
+                contents[2].substring(7).strip(),
+                Long.parseLong(contents[3].substring(7).strip()));
     }
 
-    public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        Profile profile = fileReader.getDataFromFile(new File("src/main/resources/Profile.txt"));
-        System.out.println(profile.toString());
-    }
 }
